8d6175c225dd931704e1a859a50384fdf11ae415
Harry <hjwp2@cantab.net>
2018-06-28 11:30:04 +0100

    fish config updates

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,5 +5,11 @@ set -q XDG_DATA_HOME
 
 # Load Oh My Fish configuration.
 # source $OMF_PATH/init.fish
+
+set PATH ~/.local/bin $PATH
+
 ## nb environment plugin needs unreleased version of virtualfish git+https://github.com/adambrenecki/virtualfish.git
 eval (python3 -m virtualfish auto_activation environment)
+
+set -x PYTHONDONTWRITEBYTECODE 1
+
