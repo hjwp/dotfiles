839020af4e2d85a2e46eba9d79fe92a6cdf26a94
Harry <hjwp2@cantab.net>
2019-11-08 23:46:18 +0000

    new install script additions

--- utils/new-install.sh
+++ utils/new-install.sh
@@ -3,12 +3,33 @@
 set -ex
 
 sudo apt install -y \
+    chromium-browser \
     git \
+    flatpak \
+    fonts-noto-color-emoji \
+    fonts-monofur \
+    fonts-powerline \
+    gnome-software-plugin-flatpak \
+    gnome-tweak-tool \
+    libpq-devel \
     vim \
+    vim-gtk3 \
     oathtool \
     openvpn \
-    python3.7-dev \
+    python3-dev \
+    python3-pip \
+    tmux \
+    tmate \
+    tree \
+    xclip \
+    xvfb \
     zsh
+
+# flatpak is a linux app store that has bluejeans
+
+# use resolvconf so that made vpn worsk
+sudo apt install resolvconf
+sudo resolvconf --enable-updates
 
 # docker
 sudo apt-get install -y \
@@ -22,7 +43,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   bionic
+   bionic \
    stable"
 
 # TODO: replace bionic with $(lsb_release -cs)
@@ -31,3 +52,9 @@ sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+sudo groupadd -f docker
+sudo usermod -aG docker harry
+
+# virtualenvwrapper for zsh plugin
+python3 -m pip install virtualenvwrapper
+
