607d6d72b73cb8eb364a4f52f618bf981861571a
Harry <hjwp2@cantab.net>
2023-11-21 12:29:50 +0000

    make symlinks helper

---
+++ utils/make-symlinks.sh
@@ -0,0 +1,9 @@
+ln -s ~/dotfiles/vim ~/.vim
+ln -s ~/dotfiles/config ~/.config
+ln -s ~/dotfiles/oh-my-zsh ~/.oh-my-zsh
+mkdir -p ~/.local/bin
+ln -s ~/dotfiles/utils/ping.google ~/.local/bin/ping.google
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.gvimrc ~/.gvimrc
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.zshrc ~/.zshrc
