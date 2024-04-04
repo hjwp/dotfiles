1b28516e2c411d2c0ef989b57b339756e43e439b
Harry <hjwp2@cantab.net>
2024-04-04 22:19:19 +0100

    asciidoc specific ftplugin config

---
+++ config/nvim/after/ftplugin/asciidoc.lua
@@ -0,0 +1,37 @@
+-- my own customisations for asciidoc
+
+vim.o.spell = true
+vim.o.spelllang = "en_gb"
+
+-- old book
+vim.cmd('iab <buffer> cmdg [subs="specialcharacters,quotes"]<Enter>----<Enter>$ *git')
+vim.cmd('iab <buffer> srcp <Enter>[role="sourcecode"]<Enter>.lists.tests.py (ch04l004)<Enter>====<Enter>[source,python]<Enter>----<Enter>----<Enter>====<Enter><Up><Up>')
+
+--
+--
+-- "snippets for new book
+-- "iab <buffer> srcp <Enter>[[id_here]]<Enter>.Listing title<Enter>====<Enter>[source,python]<Enter>----<Enter>----<Enter>====<Enter><Up><Up>
+-- iab <buffer> srci <Enter>++++<Enter><!-- IMAGE SOURCE<Enter>[ditaa, image_id]<Enter>....<Enter><Enter>....<Enter>--><Enter>++++<Enter>
+--
+-- "snippets for old book
+-- iab <buffer> srcp <Enter>[role="sourcecode"]<Enter>.lists.tests.py (ch04l004)<Enter>====<Enter>[source,python]<Enter>----<Enter>----<Enter>====<Enter><Up><Up>
+-- iab <buffer> srch [role="sourcecode"]<Enter>.lists/templates/home.html<Enter>[source,html]<Enter>----<Enter>
+-- iab <buffer> cmdg [subs="specialcharacters,quotes"]<Enter>----<Enter>$ *git
+-- iab <buffer> cmdt [subs="specialcharacters,macros"]<Enter>----<Enter>$ pass:quotes[*python3 manage.py test lists*]
+--
+-- vnoremap <F9> :!black -q -<CR>
+-- noremap <F10> :s/'/"/g<CR>
+-- noremap <F8> :s/)))/)))\r/g<CR>
+-- noremap <F7> :s/.home.harry.dropbox.\+superlists/...goat-book/<CR>
+-- noremap <F6> :s/.home.harry.\+.venv.\+site-packages/.../<CR>
+--
+--
+-- " snippets for tddwp
+-- iab <buffer> srcp [role="sourcecode"]<Enter>.lists/tests.py (ch00l000)<Enter>====<Enter>[source,python]<Enter>----<Enter><Enter>----<Enter>====<Enter>
+-- iab <buffer> srch [role="sourcecode"]<Enter>.lists/templates/home.html (ch00l000)<Enter>====<Enter>[source,html]<Enter>----<Enter><Enter>----<Enter>====<Enter>
+-- iab <buffer> cmdg [subs="specialcharacters,quotes"]<Enter>----<Enter>$ *git
+-- iab <buffer> cmdt [subs="specialcharacters,macros"]<Enter>----<Enter>$ pass:quotes[*python3 manage.py test lists*]
+--
+-- "snippets for new book
+-- " iab <buffer> srcp <Enter>[[id_here]]<Enter>.Listing title<Enter>====<Enter>[source,python]<Enter>----<Enter>----<Enter>====<Enter>
+-- " iab <buffer> srci <Enter>++++<Enter><!-- IMAGE SOURCE<Enter>[ditaa, image_id]<Enter>....<Enter><Enter>....<Enter>--><Enter>++++<Enter>
