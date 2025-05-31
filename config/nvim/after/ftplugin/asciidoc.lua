885a1ebf57667366e79c788ac78ac5f8ad501894
Harry <hjwp2@cantab.net>
2025-05-31 23:38:16 +0100

    fixes and fiddling in asciidoc config

--- config/nvim/after/ftplugin/asciidoc.lua
+++ config/nvim/after/ftplugin/asciidoc.lua
@@ -1,18 +1,34 @@
 -- my own customisations for asciidoc
+--
 
-vim.o.spell = true
-vim.o.spelllang = "en_gb"
+vim.opt_local.spell = true
+vim.opt_local.spelllang = "en_gb"
 
 -- old book
 vim.cmd('iab <buffer> cmdg [subs="specialcharacters,quotes"]<Enter>----<Enter>$ *git')
-vim.cmd('iab <buffer> srcp <Enter>[role="sourcecode"]<Enter>.lists.tests.py (ch04l004)<Enter>====<Enter>[source,python]<Enter>----<Enter>----<Enter>====<Enter><Up><Up>')
+vim.cmd(
+    'iab <buffer> srcp <Enter>[role="sourcecode"]<Enter>.src/lists/thing.py (ch04l004)<Enter>====<Enter>[source,python]<Enter>----<Enter>----<Enter>====<Enter><Up><Up>')
 
-vim.keymap.set("v", "<F9>",  ":!ruff format -q -<CR>")
-vim.keymap.set("n", "<F10>", "s/'/\"/g<CR>")
-vim.keymap.set("n", "<F8>",  ":s/)))/)))\\r/g<CR><BS>")
-vim.keymap.set("n", "<F7>",  ":s:\\/\\(Users\\|home\\).\\+harry.\\+workspace.\\+superlists\\/:...goat-book/:<CR>")
-vim.keymap.set("n", "<F6>",  ":s/.home.harry.\\+.venv.\\+site-packages/.../<CR>")
 
+vim.keymap.set(
+    "v", "<F9>", ":!ruff format -q -<CR>",
+    { buffer = true })
+vim.keymap.set(
+    "n", "<F10>", "s/'/\"/g<CR>",
+    { buffer = true })
+vim.keymap.set(
+    "n", "<F8>", ":s/)))/)))\\r/g<CR><BS>",
+    { buffer = true })
+vim.keymap.set(
+    "n", "<F7>", ":s:\\/\\(Users\\|home\\).\\+harry.\\+workspace.\\+superlists\\/:...goat-book/:<CR>",
+    { buffer = true })
+vim.keymap.set(
+    "n", "<F6>", ":s/.home.harry.\\+.venv.\\+site-packages/.../<CR>",
+    { buffer = true })
+
+vim.keymap.set(
+    "n", "<leader>t", "/TODO\\|HARRY\\|RITA\\|XUAN\\|DAVID\\|CSANAD\\|SEBASTIAN\\|\\<JAN\\><CR>",
+    { buffer = true })
 --
 -- "snippets for new book
 -- "iab <buffer> srcp <Enter>[[id_here]]<Enter>.Listing title<Enter>====<Enter>[source,python]<Enter>----<Enter>----<Enter>====<Enter><Up><Up>
